4610a49de6ddcc1da499fa7114d2adbfb3336428
hollycosslett <154456339+hollycosslett@users.noreply.github.com>
2023-12-30 10:45:11 +0000

    added search function using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-function searchCity(event) {
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#weather-degrees");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "48d9c02baa93fa8d733783cd33ot621f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function searchCitySubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", searchCitySubmit);
+
+searchCity("London");
